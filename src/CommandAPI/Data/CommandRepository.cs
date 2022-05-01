3d90e05f772e9da68afa9d4c0887f1b16ed2e803
enzodevgit <enzo_yair10@hotmail.com>
2022-04-30 21:23:43 -0500

    terminando fase de controlador dto mapper

--- src/CommandAPI/Data/CommandRepository.cs
+++ src/CommandAPI/Data/CommandRepository.cs
@@ -19,7 +19,11 @@ public class CommandRepository : ICommandRepository
 
     public void DeleteCommand(Command cmd)
     {
-        throw new NotImplementedException();
+        if (cmd == null)
+        {
+            throw new ArgumentNullException(nameof(cmd));
+        }
+        _context.Commands.Remove(cmd);
     }
 
     public IEnumerable<Command> GetAllCommands()
